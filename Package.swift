7f8d4635e8bfcbbf017899ba391a1a621723ec81
Rhytthm <rhytthm1717@gmail.com>
2025-02-26 16:10:24 +0530

    changing package names

--- Package.swift
+++ Package.swift
@@ -9,20 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RNetworkKit",
-            targets: ["RNetworkKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/rhytthm/RNetworkKit.git", branch: "main")
+            targets: ["RNetworkKit"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RNetworkKit",
-            path: "Sources/RNetworkKit"),
+            name: "RNetworkKit"
+        ),
         .testTarget(
             name: "RNetworkKitTests",
-            dependencies: ["RNetworkKit"]
-        ),
+            dependencies: ["RNetworkKit"])
     ]
 )
