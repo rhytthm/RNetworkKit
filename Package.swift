6373d173f6f285aeca9873dc0f2ef102bcc378ec
Rhytthm <rhytthm1717@gmail.com>
2025-02-26 17:45:39 +0530

    adding branch dependency

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "RNetworkKit",
             targets: ["RNetworkKit"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/rhytthm/RNetworkKit.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
