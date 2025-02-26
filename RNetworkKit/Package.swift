4d2bfaae17060d47ba86002be0363899d9c8936e
Rhytthm <rhytthm1717@gmail.com>
2025-02-26 12:37:18 +0530

    Creating a base project

---
+++ RNetworkKit/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RNetworkKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RNetworkKit",
+            targets: ["RNetworkKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RNetworkKit"),
+        .testTarget(
+            name: "RNetworkKitTests",
+            dependencies: ["RNetworkKit"]
+        ),
+    ]
+)
