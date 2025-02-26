9eb53c91291a779641c36001ccf63bd27204513b
Rhytthm <rhytthm1717@gmail.com>
2025-02-26 16:00:57 +0530

    PACKAGE ADDED OUTSIDE

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RNetworkKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RNetworkKit",
+            targets: ["RNetworkKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rhytthm/RNetworkKit.git", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RNetworkKit",
+            path: "Sources/RNetworkKit"),
+        .testTarget(
+            name: "RNetworkKitTests",
+            dependencies: ["RNetworkKit"]
+        ),
+    ]
+)
+
